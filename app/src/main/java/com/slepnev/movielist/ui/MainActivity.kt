a70f16b741636e505313540b6266fef774b2212a
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-04-11 21:17:02 +0300

    Now loading the posters of the movies into the RV

--- app/src/main/java/com/slepnev/movielist/ui/MainActivity.kt
+++ app/src/main/java/com/slepnev/movielist/ui/MainActivity.kt
@@ -5,12 +5,16 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.slepnev.movielist.R
+import com.slepnev.movielist.model.Movie
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainActivityViewModel
+    private val movies = arrayListOf<Movie>()
+    private val movieAdapter = MovieAdapter(movies)
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +26,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initViews() {
+        rvMovies.layoutManager = GridLayoutManager(this@MainActivity, 2)
+        rvMovies.adapter = movieAdapter
+
         btnSubmit.setOnClickListener {
             viewModel.getMovies(etYear.text.toString())
         }
@@ -30,10 +37,9 @@ class MainActivity : AppCompatActivity() {
     private fun initViewModel() {
         viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
         viewModel.moviesPage.observe(this, Observer {
-            it?.movies?.forEach {
-                var old = tvPopularMovies.text
-                tvPopularMovies.text = "$old \n ${it.title}"
-            }
+            movies.clear()
+            movies.addAll(it.movies)
+            movieAdapter.notifyDataSetChanged()
         })
         viewModel.error.observe(this, Observer {
             Toast.makeText(this, it, Toast.LENGTH_LONG).show()
