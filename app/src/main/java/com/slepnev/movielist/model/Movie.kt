74de3822099332498aa5026544c3edd672e8e102
Dmitrii Slepnev <odmsl002@edu.xamk.fi>
2020-04-11 17:21:26 +0300

    Created movie data model

---
+++ app/src/main/java/com/slepnev/movielist/model/Movie.kt
@@ -0,0 +1,15 @@
+package com.slepnev.movielist.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+    @SerializedName("title") var title: String,
+    @SerializedName("backdrop_path") var backdropImage: String,
+    @SerializedName("poster_path") var posterImage: String,
+    @SerializedName("release_date") var releaseDate: String,
+    @SerializedName("vote_average") var rating: Int,
+    @SerializedName("overview") var overview: String
+) {
+    fun getBackdropUrl() = "https://image.tmdb.org/t/p/w185/$backdropImage"
+    fun getPosterUrl() = "https://image.tmdb.org/t/p/w185/$posterImage"
+}
